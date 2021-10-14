57bf608f8cb2ad1a258879a4ab980fbb85c962c5
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-10-14 16:57:36 +0200

    Create image gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,87 @@
+import React, { Component } from "react";
+import fetchImages from "./services/api";
+import Searchbar from "./components/Searchbar/Searchbar";
+import Loader from "./components/Loader/Loader";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
+
+class App extends Component {
+  state = {
+    searchQuery: "beagle",
+    page: 1,
+    listImages: null,
+    loading: false,
+    showModal: false,
+  };
+
+  componentDidMount() {
+    fetchImages(this.state.searchQuery)
+      .then((data) => this.setState({ listImages: data }))
+      .catch((err) => console.log("Error:", err));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
 
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+      return fetchImages(this.state.searchQuery, 1)
+        .then((data) => {
+          this.setState({ listImages: data, page: 1 });
+        })
+        .catch((err) => console.log("Error:", err));
+    }
+
+    if (prevState.page !== this.state.page && this.state.page !== 1) {
+
+      return fetchImages(this.state.searchQuery, this.state.page)
+        .then((data) => {
+          this.setState((prevState) => ({
+            listImages: [...prevState.listImages, ...data],
+          }));
+
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+          })
+        })
+
+        .catch((err) => console.log("Error:", err));
+    }
+  }
+
+  handleSubmit = (searchQuery) => {
+    this.setState({ searchQuery });
+  };
+
+  loadMore = () => {
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  };
+
+  openModal = (imageURL) => {
+    this.setState({ largeImage: imageURL, showModal: true });
+  };
+
+  onClose = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { listImages, loading, showModal } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit} />
+        {listImages && (
+          <ImageGallery listImages={listImages} onClick={this.openModal} />
+        )}
+        {loading && <Loader />}
+        {listImages && <Button onClick={this.loadMore} />}
+        {showModal && (
+          <Modal url={this.state.largeImage} onClose={this.onClose} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
